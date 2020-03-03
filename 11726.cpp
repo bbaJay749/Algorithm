6e25cf2df8d0227f55c3fb09a5fa360bb162abd6
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-03-03 16:33:52 +0900

    Create 11726.cpp

---
+++ 11726.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+/* CONSIDER d[n] = d[n-2] + d[n-1] */
+int main() {
+    int d[1001] = { 0 };
+    int n = 0;
+
+    cin >> n;
+    
+    d[1] = 1;
+    d[2] = 2;
+    
+    for (int i = 3; i <= n; i++) {
+        d[i] = (d[i - 2] + d[i - 1]) % 10007;
+    }
+        
+    cout << d[n] << endl;
+}
+
+
+/* revised from https://suriisurii.tistory.com/38 */
