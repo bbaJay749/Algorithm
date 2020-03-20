4b2278f179338e55dcd54c842e3e824d0d9e499b
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-03-20 20:54:31 +0900

    Update 2193_이친수.cpp

--- 2193_이친수.cpp
+++ 2193_이친수.cpp
@@ -25,3 +25,66 @@ int main() {
 	return 0;
 }
 
+/* Dumb way
+int main() {
+    int N = 0, totalnum = 0;
+    cin >> N;
+    totalnum = pow(2, N);
+
+    vector<vector<bool>> Pinary(pow(2, N), vector<bool> (N, 0));
+
+    for (int i = 1; i < pow(2, N); i++) {
+        bool flag = 0;
+        Pinary[i] = Pinary[i - 1];
+        putnum(Pinary, i, 0, flag);
+    }
+
+    for (int i = 0; i < pow(2, N); i++) {
+        for (int k = 0; k < N; k++) {
+            cout << Pinary[i][k];
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < pow(2, N); i++) {
+        bool flag = 0;
+        for (int k = 1; k < N; k++) {
+            if (flag == 0) {
+                if (Pinary[i][0] == 0) {
+                    totalnum--;
+                    flag = 1;
+                }
+                else if (Pinary[i][k] == 1 && Pinary[i][k - 1] == 1) {
+                    totalnum--;
+                    flag = 1;
+                }
+            }
+            /*if (flag == 1) {
+                if (Pinary[i][k] == 1 && Pinary[i][k - 1] == 1) {
+                    totalnum++;
+                    break;
+                }
+            }*/
+        }
+    }
+
+    cout << totalnum << endl;
+
+    return 0;
+}
+
+void putnum(vector<vector<bool>> &Pinary, int i, int k, int flag) {
+    if (flag == 1) return;
+
+    if (Pinary[i][k] == 0) {
+        Pinary[i][k] = 1;
+        flag = 1;
+        return;
+    }
+    else {
+        Pinary[i][k] = 0;
+        putnum(Pinary, i, k + 1, flag);
+    }
+}
+*/
+
