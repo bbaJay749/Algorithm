98800af060358c2a30c6d495c2bdeba63e85f9f0
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-03-24 15:26:01 +0900

    Update 2631_줄세우기.cpp

--- 2631_줄세우기.cpp
+++ 2631_줄세우기.cpp
@@ -1,3 +1,46 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+using namespace std;
+
+int cal(vector<int> children, int N);
+
+int main() {
+	int N = 0, counter = 0;
+	cin >> N;
+	vector<int> children(N, 0);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &children[i]);
+	}
+
+	cout << N - cal(children, N) << endl;
+
+	return 0;
+}
+
+int cal(vector<int> children, int N) {   // find the size of the LIS: Longest Increasing Subsequence
+	vector<int> LIS(N, 0);
+	int max = 1;
+
+	LIS[0] = 1;
+	for (int i = 0; i < N; i++) {
+		LIS[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (children[j] < children[i] && LIS[j] + 1 > LIS[i]) {
+				LIS[i] = LIS[j] + 1;
+			}
+		}
+
+		if (max < LIS[i]) {
+			max = LIS[i];
+		}
+	}
+
+	return max;
+}
+
 /* Dumb way cannot beat the time limit 
 
 #include <cstdio>
