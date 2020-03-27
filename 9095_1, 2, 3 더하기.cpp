6847310b92885aee3cb20c8cce5542a7304f99e8
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-03-27 17:28:15 +0900

    Update 9095_1, 2, 3 더하기.cpp

--- 9095_1, 2, 3 더하기.cpp
+++ 9095_1, 2, 3 더하기.cpp
@@ -36,6 +36,20 @@ void cal(int DP[]) {
 #include <iostream>
 using namespace std;
 
+int getSum(int n, int cnt){
+	if(n == 0){ // n == 0: 1,2,3으로 이루어진 더하기 모두 완성
+		return cnt+1;
+	}
+	
+	for(int i = 1; i <= 3; i++){
+		if(n-i >= 0){ // n-i가 0보다 작으면 최종적으로 n보다 큰 덧셈 만들어짐, 따라서 조건문으로 걸러줌
+			cnt = getSum(n-i,cnt); // +i로 만들어주고 n에서 i빼줌(이를 재귀적으로 수행)
+		}
+	}
+	
+	return cnt;
+}
+
 int main() {
 	int T;
 	cin >> T;
@@ -43,20 +57,9 @@ int main() {
 		int n;
 		cin >> n;
 		
-		int arr[n+1];
-		arr[0] = 1; // arr[0] = 1은 초기값
-		
-		for(int i = 1; i < 4; i++){ // 1이 들어가는 경우의 수부터 3이 들어가는 경우의 수까지 따지기 위함
-			for(int j = i; j < n+1; j++){ // 현재 i보다 작은 값은 i로 만들 수 없기 때문에 i부터 시작해야함
-				arr[j] = 0;
-				for(int k = 1; k <= i; k++){ // 이전 i개의 배열 값을 더해줌
-					arr[j] += arr[j-k];
-				}
-			}
-		}
-		
-		cout << arr[n] << '\n';
+		cout << getSum(n,0) << "\n";
 	}
+	
 	return 0;
 }
 */
