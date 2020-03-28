229fec8630b7d809a7b403d7b5d1b3e3c79c991a
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-03-28 13:52:56 +0900

    Update 2529_부등호.cpp

--- 2529_부등호.cpp
+++ 2529_부등호.cpp
@@ -12,11 +12,11 @@ int main() {
 	int N = 0;
 	cin >> N;
 
-	vector<int> inequalities;
-	vector<int> num_in_row;
-	vector<bool> max_num_use(10, 1);
+	vector<int> inequalities;	// input
+	vector<int> num_in_row;		// count how many < or > in a row
+	vector<bool> max_num_use(10, 1);	// numbers used : 1
 	vector<bool> min_num_use(10, 1);
-	vector<int> maxarray(N + 1, 0);
+	vector<int> maxarray(N + 1, 0);		// output num
 	vector<int> minarray(N + 1, 0);
 
 	for (int i = 0; i < N; i++) {
@@ -32,20 +32,20 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		if (inequalities[i] == 60) {
-			maxarray[i] = nthmax(max_num_use, num_in_row[i]);
+			maxarray[i] = nthmax(max_num_use, num_in_row[i]);	// find nth max num
 			minarray[i] = nthmin(min_num_use, 0);
-			max_num_use[maxarray[i]] = 0;
+			max_num_use[maxarray[i]] = 0;	// make nth max num into used one
 			min_num_use[minarray[i]] = 0;
 		}
 		else if (inequalities[i] == 62) {
 			maxarray[i] = nthmax(max_num_use, 0);
-			minarray[i] = nthmin(min_num_use, num_in_row[i]);
-			max_num_use[maxarray[i]] = 0;
-			min_num_use[minarray[i]] = 0;
+			minarray[i] = nthmin(min_num_use, num_in_row[i]);	// find nth min num
+			max_num_use[maxarray[i]] = 0;	
+			min_num_use[minarray[i]] = 0;	// make nth min num into used one
 		}
 	}
 	maxarray[N] = nthmax(max_num_use, 0);
-	minarray[N] = nthmin(min_num_use, 0);
+	minarray[N] = nthmin(min_num_use, 0);	//last digits 
 
 	for (int i = 0; i <= N; i++) {
 		cout << maxarray[i];
@@ -59,7 +59,8 @@ int main() {
 	return 0;
 }
 
-int count_num(vector<int> inequalities, int i, int N) {
+/* count how many < or > are in a row */
+int count_num(vector<int> inequalities, int i, int N) {	
 	int num = 1;
 
 	for (int k = i; k < N - 1; k++) {
@@ -72,16 +73,16 @@ int count_num(vector<int> inequalities, int i, int N) {
 	return num;
 }
 
+/* find nth max value among available digits */
 int nthmax(vector<bool> max_num_use, int i) {
 	int count = 0;
 
 	for (int k = 9; k >= 0; k--) {
-		if (count == i && max_num_use[k]) {
+		if (count == i && max_num_use[k]) {	// if the digit is not used and it's nth max value
 			return k;
 		}
-		if (max_num_use[k]) count++;
+		if (max_num_use[k]) count++;	// if the digit is not used
 	}
-
 }
 
 int nthmin(vector<bool> min_num_use, int i) {
