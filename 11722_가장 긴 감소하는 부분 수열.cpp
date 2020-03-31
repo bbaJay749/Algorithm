42b0c40d4ab38683f85ac70a15863022fe376d13
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-03-31 18:00:15 +0900

    Create 11722_가장 긴 감소하는 부분 수열.cpp

---
+++ 11722_가장 긴 감소하는 부분 수열.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+	int N = 0;
+	cin >> N;
+
+	vector<int> nums(N, 0);
+	for (int i = 0; i < N; i++) {
+		cin >> nums[i];
+	}
+
+	int max = 1;
+
+	vector<int> LDS(N, 0);
+	LDS[0] = 1;
+	for (int i = 0; i < N; i++) {
+		LDS[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (nums[j] > nums[i] && LDS[j] + 1 > LDS[i]) {
+				LDS[i]++;
+			}
+		}
+
+		if (max < LDS[i]) {
+			max = LDS[i];
+		}
+	}
+
+	cout << max;
+
+	return 0;
+}
