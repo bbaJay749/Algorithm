6d3e4e56e15f95880a1040d9e3d76a0c23f9c6f2
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-05-19 17:04:38 +0900

    Create 1495_기타리스트.cpp

---
+++ 1495_기타리스트.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int NumofSongs, InitVol, MaxVol;
+int result = -1;
+vector<int> Vols;
+vector<vector<bool> > dp(101, vector<bool>(1001, false));
+
+void dfs(int index, int tempVol) {
+    if (tempVol < 0 || tempVol > MaxVol) {
+        return;
+    }
+
+    /* this is necessary to remove redundant cases, there can be cases like this 
+       
+       (Order, Volume)
+       (0, 5) -> (1, 6) -> (2, 7)
+                        -> (2, 5) which is not available case
+              -> (1, 4) -> (2, 5) then this case doesn't even need to be calculated 
+                        -> (2, 3) this case need to be calculateds
+    */
+    if (dp[index][tempVol]) {
+        return;
+    }
+
+    if (index == NumofSongs) {
+        result = max(result, tempVol);
+        return;
+    }
+
+    dp[index][tempVol] = true;
+
+    dfs(index + 1, tempVol + Vols[index]);
+    dfs(index + 1, tempVol - Vols[index]);
+}
+
+
+int main() {
+    cin >> NumofSongs >> InitVol >> MaxVol;
+   
+    int tempvol;
+    for (int i = 0; i < NumofSongs; i++) {
+        cin >> tempvol;
+        Vols.push_back(tempvol);
+    }
+
+    dfs(0, InitVol);
+
+    cout << result << "\n";
+
+    return 0;
+}
