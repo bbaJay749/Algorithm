9823100eafe7141f9c0497417bfa730598d339d8
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-05-28 14:30:08 +0900

    Create 6198_옥상 정원 꾸미기.cpp

---
+++ 6198_옥상 정원 꾸미기.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int N;
+vector<int> buildings;
+stack<int> por_count;
+long long int result = 0;
+	
+int main() {
+	cin >> N;
+
+	int temp = 0;
+	for (int i = 0; i < N; i++) {
+		cin >> temp;
+		buildings.push_back(temp);
+	}
+
+	for (int i = 0; i < N; i++) {
+		while (!por_count.empty() && por_count.top() <= buildings[i]) {
+			por_count.pop();
+		}
+	
+		por_count.push(buildings[i]);
+
+		result += por_count.size() - 1;
+	}
+
+	cout << result << "\n";
+
+	return 0;
+}
+
+/* revised from https://jaimemin.tistory.com/1363 */
