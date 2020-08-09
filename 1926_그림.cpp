bc1ead6af8726d33073f45008e1a0f26f225c57f
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-08-09 22:02:25 +0900

    Update 1926_그림.cpp

--- 1926_그림.cpp
+++ 1926_그림.cpp
@@ -8,18 +8,20 @@ int num_drawings;
 int size_drawing, L_size_drawing;
 bool flag, dot;
 
-void getdrawing(bool drawings[501][501], bool checker[501][501], int i, int j) {
+void getdrawing(bool drawings[500][500], bool checker[500][500], int i, int j) {
     if (!checker[i][j]) {
         checker[i][j] = true;
         if (drawings[i][j]) {
             if (!flag) {
-                flag = true; num_drawings++;
+                flag = true; 
+                num_drawings++;
             }
             L_size_drawing++;
+
             if (0 < i) getdrawing(drawings, checker, i - 1, j);
-            if (i < size_v) getdrawing(drawings, checker, i + 1, j);
+            if (i < size_v - 1) getdrawing(drawings, checker, i + 1, j);
             if (0 < j) getdrawing(drawings, checker, i, j - 1);
-            if (j < size_h) getdrawing(drawings, checker, i, j + 1);
+            if (j < size_h - 1) getdrawing(drawings, checker, i, j + 1);
         }
     }
     else;
@@ -28,9 +30,9 @@ void getdrawing(bool drawings[501][501], bool checker[501][501], int i, int j) {
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    
-    bool drawings[501][501] = { 0 };
-    bool checker[501][501] = { 0 };
+
+    bool drawings[500][500] = { 0 };
+    bool checker[500][500] = { 0 };
     cin >> size_v >> size_h;
 
     for (int i = 0; i < size_v; i++) {
@@ -42,7 +44,8 @@ int main() {
 
     for (int i = 0; i < size_v; i++) {
         for (int j = 0; j < size_h; j++) {
-            L_size_drawing = 0; flag = 0;
+            L_size_drawing = 0;
+            flag = 0;
             getdrawing(drawings, checker, i, j);
             if (L_size_drawing > size_drawing) size_drawing = L_size_drawing;
         }
