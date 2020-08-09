c15b1e41e05659aabf290adffc86aff80f0e9f77
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-08-09 21:17:25 +0900

    Update 1722_순열의 순서.cpp

--- 1722_순열의 순서.cpp
+++ 1722_순열의 순서.cpp
@@ -5,62 +5,45 @@
 using namespace std;
 
 typedef unsigned long long ull;
-void setFac(ull fac[]);
-void getpermut(ull fac[], int N);
-void getorder(ull fac[], int N);
+vector<ull> fac(21, 0);
+int N, M;
 
-int main() {
-	ull fac[21] = {1, };
-	int N = 0, M = 0;
-
-	setFac(fac);
-	cin >> N >> M;
-
-	if (M == 1) {
-		getpermut(fac, N);
-	}
-	else if (M == 2) {
-		getorder(fac, N);
-	}
-
-	return 0;
-}
-
-void setFac(ull fac[]) {
-	for (int i = 1; i <= 20; ++i) {
+void setFac() {
+	fac[0] = 1;
+	for (int i = 1; i <= 20; i++) {
 		fac[i] = fac[i - 1] * i;
 	}
 }
 
-void getpermut(ull fac[], int N) {
+void getpermut(int N) {
 	ull k = 0;
-	cin >> k;
+	cin >> k; // K th permut
 
 	vector<int> initial;
-	for (int i = 1; i <= N; ++i) {
+	for (int i = 1; i <= N; i++) {
 		initial.push_back(i);
 	}
 
 	vector<int> ans;
-	
-	--k;
+
+	k--;
 	int p = N - 1;
 	while (p >= 0) {
-		int div = k / fac[p];
+		int div = k / fac[p];	// N / (p!)
 		ans.push_back(initial[div]);
 		initial.erase(initial.begin() + div); // erase checked vector 
-		k = k % fac[p];
-		--p;
+		k %= fac[p];
+		p--;
 	}
 
-	for (int i = 0; i < N; ++i) {
+	for (int i = 0; i < N; i++) {
 		cout << ans[i] << " ";
 	}
 	cout << endl;
 }
 
-void getorder(ull fac[], int N) {
-	vector<int> permut(N + 1, 0);	
+void getorder(int N) {
+	vector<int> permut(N + 1, 0);
 	ull order = 1; // the last one ++
 
 	for (int i = 1; i <= N; i++) {
@@ -78,4 +61,15 @@ void getorder(ull fac[], int N) {
 	}
 
 	cout << order << endl;
+}
+
+int main() {
+	setFac(); // put 1! ~ 20!
+
+	cin >> N >> M;
+
+	if (M == 1) getpermut(N);
+	else if (M == 2) getorder(N);
+
+	return 0;
 }
