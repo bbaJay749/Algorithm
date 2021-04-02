31c06080ebe3392fde3ac9f8d8f8e0fdda050d37
Jaeyong Park <pjy101102@gmail.com>
2021-04-02 14:46:50 +0900

    [Solve] Programmers_전화번호 목록

---
+++ Progammers_주식가격.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    for(int a = 0; a < prices.size(); a++){
+        int cnt = 0;
+        for(int b = a + 1; b < prices.size(); b++){
+            if(prices[b] >= prices[a]){
+                cnt++;
+            }
+            else{
+                cnt++;
+                break;
+            }
+        }
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
