85c784af6b8696ea00b2291522084d4277de3c6c
Jaeyong Park <pjy101102@gmail.com>
2021-04-02 14:48:17 +0900

    [Solve] Programmers_주식가격

--- Progammers_주식가격.cpp
+++ Progammers_주식가격.cpp
@@ -6,13 +6,13 @@ using namespace std;
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
     
-    for(int a = 0; a < prices.size(); a++){
+    for(int a = 0; a < prices.size(); a++) {
         int cnt = 0;
-        for(int b = a + 1; b < prices.size(); b++){
-            if(prices[b] >= prices[a]){
+        for(int b = a + 1; b < prices.size(); b++) {
+            if(prices[b] >= prices[a]) {
                 cnt++;
             }
-            else{
+            else {
                 cnt++;
                 break;
             }
