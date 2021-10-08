6a767e92664916cdc7e4c51f22a79cf3826fb336
Jaeyong Park <jy.park@koreacb.com>
2021-10-08 15:56:41 +0900

    [Solve] 나동빈_음료수 얼려 먹기

---
+++ 나동빈_음료수 얼려 먹기.py
@@ -0,0 +1,31 @@
+# get N, M
+n, m = map(int, input().split())
+
+# generate map
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    
+    # if never visit
+    if graph[x][y] == 0:
+        # check visit
+        graph[x][y] = 1
+        # call all EWSN directions
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
