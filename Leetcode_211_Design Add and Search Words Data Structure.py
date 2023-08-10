f49cb6436a42c946cdcbc5fbc4daf482e19a6083
Jaeyong Park <pjy101102@gmail.com>
2023-08-10 16:08:43 +0900

    [SUP] Leetcode_211_Design Add and Search Words

--- Leetcode_211_Design Add and Search Words Data Structure.py
+++ Leetcode_211_Design Add and Search Words Data Structure.py
@@ -37,3 +37,37 @@ class WordDictionary:
                 if pattern.search(word_from_dict):
                     return True
             return False
+
+
+''' Better way using trie
+
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = dict()
+
+    def addWord(self, word: str) -> None:
+        node = self.trie
+        for ch in word + '🔚':
+            if ch not in node:
+                node[ch] = dict()
+
+            node = node[ch]
+
+    def search(self, word: str) -> bool:
+        nodes = [self.trie]
+        for ch in word + '🔚':
+            newNodes = []
+            for node in nodes:
+                if ch == '.':
+                    newNodes += [v for v in node.values()]
+                elif ch in node:
+                    newNodes.append(node[ch])
+
+            if not newNodes:
+                return False
+
+            nodes = newNodes
+
+        return True
+'''
